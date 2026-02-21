c3343288a5451f4c37d297f39bafedf5bf5fbab1
brotherguns <braydenr209@gmail.com>
2026-02-21 16:46:58 -0600

    Create rollup.config.mjs

---
+++ rollup.config.mjs
@@ -0,0 +1,21 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { swc } from "rollup-plugin-swc3";
+
+export default {
+    input: "src/index.js",
+    output: {
+        file: "dist/index.js",
+        format: "cjs",
+        exports: "default"
+    },
+    plugins: [
+        nodeResolve(),
+        swc({
+            env: { targets: "defaults" },
+            jsc: {
+                parser: { syntax: "ecmascript" },
+                target: "es2022"
+            }
+        })
+    ]
+};
